7527bd4b25d64a75fd9af13edcac9a40042cf88b
MDBluequasar <134247507+MDBluequasar@users.noreply.github.com>
2023-06-06 16:38:55 +0900

    Update study_02_02.py

---
+++ study_02_02.py
@@ -0,0 +1,16 @@
+99 페이지 
+'1이 될 때까지'
+ 
+n = int(input('값을 입력해주세요.: '))
+k = int(input('값을 입력해주세요.: '))
+ans = 0
+while True:
+    y = (n // k) * k
+    ans += (n - y)
+    n = y
+    if n < k:
+        break
+    ans += 1
+    n //= k
+ans += (n - 1)
+print(ans)
